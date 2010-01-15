7fa82e77270568483745c70c6dab5a4bff865f64
fukuchi <none@none>
2010-01-15 22:23:27 +0000

    --HG-- branch : lyq extra : convert_revision : svn%3A0e2b8f54-fac9-0310-8689-826315a91ed8/works/lyq%402635

--- twitter_oauthpin.js
+++ twitter_oauthpin.js
@@ -1,12 +1,19 @@
 (function () {
-	var pin = document.getElementById("oauth_pin").innerText;
+ 	var msg = document.getElementsByClassName("message-content")[0];
+	var msg_text = msg.textContent;
+
+	if (!msg_text.match(/Lyq/) || msg_text.match(/denied/i)) return;
+
+	var pin = document.getElementById("oauth_pin").textContent;
+	msg.children[1].textContent = "Now Lyq is reading the PIN automatically";
+
 	chrome.extension.sendRequest({"request": "twitter_pin", "pin": pin},
 		function(resp) {
 			var content = document.getElementById("content");
 			if(resp) {
 				content.innerHTML = "<div class='message-content'><h2>Congratulations, Lyq is successfully authenticated.</h2><p>You can close this tab.</p></div>";
 			} else {
-				content.innerHTML = "<div class='message-content'><h2>Failed to authenticate Lyq.</h2><p>Please try again. You can close this tab.</p></div>";
+				content.innerHTML = "<div class='message-content'><h2>Failed to authenticate Lyq.</h2><p>Please close this tab and try to sign in again.</p></div>";
 			}
 	});
 }());
