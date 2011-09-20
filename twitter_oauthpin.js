127d94ac69a3b4b4623599b207409046eafa73e5
kentaro@fukuchi.org <kentaro@fukuchi.org>
2011-09-21 02:44:29 +0900

    Updated PIN retriever.
    --HG--
    branch : lyq

--- twitter_oauthpin.js
+++ twitter_oauthpin.js
@@ -1,19 +1,19 @@
 (function () {
- 	var msg = document.getElementsByClassName("message-content")[0];
-	var msg_text = msg.textContent;
+	var msg = $("#oauth_pin p")
+	var msg_text = $("#bd").text();
 
 	if (!msg_text.match(/Lyq/) || msg_text.match(/denied/i)) return;
 
-	var pin = document.getElementById("oauth_pin").innerText.replace(/^(\s|\u00A0)+|(\s|\u00A0)+$/g,"");
-	msg.children[1].textContent = "Now Lyq is reading the PIN automatically. (PIN: " + pin + ")";
+	var pin = $.trim($("code").text());
+	msg.html("<p>Now Lyq is reading the PIN automatically. (PIN: " + pin + ")</p>");
 
 	chrome.extension.sendRequest({"request": "twitter_pin", "pin": pin},
 		function(resp) {
-			var content = document.getElementById("content");
+			var content = document.getElementById("oauth_pin");
 			if(resp) {
-				content.innerHTML = "<div class='message-content'><h2>Congratulations, Lyq is successfully authenticated.</h2><p>You can close this tab.</p></div>";
+				content.innerHTML = "<div><h2>Congratulations, Lyq is successfully authenticated.</h2><p>You can close this tab.</p></div>";
 			} else {
-				content.innerHTML = "<div class='message-content'><h2>Failed to authenticate Lyq.</h2><p>Please close this tab and try to sign in again.</p></div>";
+				content.innerHTML = "<div><h2>Failed to authenticate Lyq.</h2><p>Please close this tab and try to sign in again.</p></div>";
 			}
 	});
 }());
